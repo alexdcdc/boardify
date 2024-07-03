60111a2ffab704b93626197e0460606857af5540
alexdcdc <69087503+alexdcdc@users.noreply.github.com>
2024-07-03 18:30:43 -0400

    add functionality to generate multiple board cards, adjust card formatting

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 import Card from '../../components/Card';
 
 export default function Home() {
+    const numCards = 20;
+    const nameList = [];
+    for (let i = 1; i <= numCards; i++) {
+        nameList.push(`Example Card ${i}`)
+    }
     return (
-        <div className="flex justify-center items-center min-h-screen bg-gray-100">
-            <Card
-            posterName="Poster Name"
-            editedHoursAgo="x"
-            imageUrl="https://tailwind-generator.b-cdn.net/images/card-generator/tailwind-card-generator-card-preview.png"
-            />
-       </div>
+        <div className="flex flex-wrap items-start min-h-screen bg-gray-100">
+            {nameList.map((name, i) =>
+                <Card
+                    key = {i}
+                    posterName={name}
+                    editedHoursAgo="x"
+                    imageUrl="https://tailwind-generator.b-cdn.net/images/card-generator/tailwind-card-generator-card-preview.png"
+                />
+            )}
+        </div>
+
     );
-  }
+}
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,15 +11,15 @@ interface CardProps {
 // Create the Card component
 const Card: React.FC<CardProps> = ({ posterName, editedHoursAgo, imageUrl }) => {
     return (
-        <div className="flex flex-col rounded-2xl w-96 bg-[#ffffff] shadow-xl">
-            <figure className="flex justify-center items-center rounded-2xl grow">
+        <div className="flex flex-col rounded-2xl w-96 bg-[#ffffff] shadow-xl m-2">
+            <figure className="flex justify-center items-center rounded-2xl">
                 <img
                     src={imageUrl}
                     alt="Card Preview"
                     className="rounded-t-2xl"
                 />
             </figure>
-            <div className="flex flex-col grow-0 p-8">
+            <div className="flex flex-col p-8">
                 <div className="text-2xl font-bold text-[#374151] pb-6">{posterName}</div>
                 <div className="text-lg text-[#374151]">Edited {editedHoursAgo} hours ago</div>
                 <div className="flex justify-end pt-6">
